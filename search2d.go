077a1e7ef3ea3023dd1d8515ea9eaa02e6c5f199
jchenriquez <jc.henriquez04@gmail.com>
2020-02-06 18:23:21 -0500

    doing binary search by reducing the matrix didn't quite work reducing to iterating rows and binary searching on columns.

--- search2d.go
+++ search2d.go
@@ -1,48 +1,26 @@
 package search2d
 
-func search2D (matrix [][]int, target, rowStart, rowEnd, columnStart, columnEnd int) bool {
-  if rowEnd - rowStart == 0 && columnEnd - columnStart == 0 {
-    return matrix[rowStart][columnStart] == target
-  }
+import (
+	"sort"
+)
 
-  columnMid := (columnEnd+columnStart) / 2
-	rowMid := (rowEnd+rowStart) / 2
-
-	var newColumnStart int
-	var newColumnEnd int
-	var newRowStart int
-	var newRowEnd int
-
-	if matrix[rowStart][columnMid] == target {
-		return true
-	}
-
-	if matrix[rowMid][columnStart] == target {
-		return true
-	}
-
-	if target > matrix[rowStart][columnMid] {
-		newColumnStart = columnMid
-		newColumnEnd = columnEnd
-	} else {
-		newColumnStart = columnStart
-		newColumnEnd = columnMid
-	}
-
-	if target > matrix[columnMid][rowMid] {
-		newRowStart = rowMid
-		newColumnEnd = rowEnd
-	} else {
-		newRowStart = rowStart
-		newRowEnd = rowMid
-	}
-
-  return search2D(matrix, target, newRowStart, newRowEnd, newColumnStart, newColumnEnd)
-}
-
+//SearchMatrix will search a target in a matrix using binary search.
 func SearchMatrix(matrix [][]int, target int) bool {
-  if len(matrix) == 0{
-    return false
-  }
-  return search2D(matrix, target, 0, len(matrix), 0, len(matrix[0]))
+	if len(matrix) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		column := matrix[i]
+
+		index := sort.Search(len(column), func(j int) bool {
+			return column[j] >= target
+		})
+
+		if index < len(column) && column[index] == target {
+			return true
+		}
+	}
+
+	return false
 }
--- search2d_test.go
+++ search2d_test.go
@@ -1,41 +1,48 @@
 package search2d
 
-import "testing"
-import "encoding/json"
-import "os"
-import "bufio"
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"os"
+	"testing"
+)
 
 type Test struct {
-  Matrix [][] int `json:"matrix"`
-  Targets []int `json:"targets"`
-  Output []bool `json:"output"`
+	Matrix  [][]int `json:"matrix"`
+	Targets []int   `json:"targets"`
+	Output  []bool  `json:"output"`
 }
 
-func TestSearchMatrix (tst *testing.T) {
-  f, err := os.Open("./test.json")
+func TestSearchMatrix(tst *testing.T) {
+	f, err := os.Open("./test.json")
 
-  if err != nil {
-    tst.Error(err)
-  }
+	if err != nil {
+		tst.Error(err)
+	}
 
-  defer f.Close()
+	defer f.Close()
 
-  reader := bufio.NewReader(f)
-  decoder := json.NewDecoder(reader)
-  var tests map[string]Test
+	reader := bufio.NewReader(f)
+	decoder := json.NewDecoder(reader)
+	var tests map[string]Test
 
-  for {
-    err := decoder.Decode(&tests)
-    if err == nil {
-      for name, test := range tests {
-        tst.Run(name, func (st *testing.T) {
-          for i := 0; i < len(test.Targets); i++ {
-            if SearchMatrix(test.Matrix, test.Targets[i]) != test.Output[i] {
-              st.Errorf("target %d expected %v\n", test.Targets[i], test.Output[i])
-            }
-          }
-        })
-      }
-    }
-  }
+	for {
+		err := decoder.Decode(&tests)
+		if err == nil {
+			for name, test := range tests {
+				tst.Run(name, func(st *testing.T) {
+					for i := 0; i < len(test.Targets); i++ {
+						if SearchMatrix(test.Matrix, test.Targets[i]) != test.Output[i] {
+							st.Errorf("target %d expected %v\n", test.Targets[i], test.Output[i])
+						}
+					}
+				})
+			}
+		} else if err == io.EOF {
+			break
+		} else {
+			tst.Error(err)
+		}
+	}
 }
